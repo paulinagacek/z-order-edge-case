9787333af51292e598b015e84b102e3338676b5e
Paulina Gacek <paulinagacek@google.com>
2025-11-13 13:00:34 +0000

    child window button

--- public/src/child.js
+++ public/src/child.js
@@ -5,12 +5,17 @@ const id = urlParams.get('id');
 
 const h1 = document.getElementById('windowTitle');
 const p = document.getElementById('windowText');
+const btn_focus = document.getElementById('windowText');
 
 if (id) {
     h1.textContent = `Window ${id}`;
     p.textContent = `This is child window number ${id}.`;
     document.title = `Child Window ${id}`;
     console.log(`child.html: Content updated for Window ${id}`);
+    if (id == 1){
+        btn_focus.style.display = 'flex';
+    }
+
 } else {
     console.log('child.html: No ID found in URL parameters.');
 }
